fd8ec04c7720adab3cf8a8c0f36e89b1916ed965
Ayush Tripathi <ayush@Ayushs-MacBook-Air-2.local>
2024-08-23 16:19:36 +0530

    updated Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -18,7 +18,7 @@ const Footer = () => {
 
       <div className="flex flex-col items-center">
         <h1 className="heading lg:max-w-[45vw]">
-          I'm free to work   <span className="text-purple">and contribute in projects and more</span> 
+          I&apos;m free to work   <span className="text-purple">and contribute in projects and more</span> 
         </h1>
         <p className="text-white-200 md:mt-10 my-5 text-center">
           Reach out to me today and let&apos;s discuss how I can help you
@@ -26,7 +26,7 @@ const Footer = () => {
         </p>
         <a href="mailto:ayushmani26@gmail.com">
           <MagicButton
-            title="Let's get in touch"
+            title="Let&apos;s get in touch"
             icon={<FaLocationArrow />}
             position="right"
           />
@@ -39,12 +39,16 @@ const Footer = () => {
 
         <div className="flex items-center md:gap-3 gap-6">
           {socialMedia.map((info) => (
-            <div
-              key={info.id}
-              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
-            >
+            <a
+            key={info.id}
+            href={info.link} // Link URL
+            target="_blank" // Open link in a new tab
+            rel="noopener noreferrer" // Security for new tab
+          >
+            <div className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300">
               <img src={info.img} alt="icons" width={20} height={20} />
             </div>
+          </a>
           ))}
         </div>
       </div>
